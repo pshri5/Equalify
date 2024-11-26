17b7c93c040d53eb799fba8e3198165f9ada17f0
ankugargdev619 <ankugargdev619@gmail.com>
2024-11-26 06:14:50 +0530

    Login page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,27 @@ import './App.css'
 import { Button } from './ui/components/Button';
 import { ShareIcon } from './ui/icons/ShareIcon';
 import { Input } from './ui/components/Input';
+import { Card } from './ui/components/Card';
 
 function App() {
 
   return (
     <>
       <div className='flex items-center justify-center h-screen flex-col gap-8'>
-        <Input placeholder="Enter your email"/>
-        <Input placeholder="Enter your password" type="password"/>
-        <Button size="sm" variant="outline" label="Share" startIcon={<ShareIcon />} endIcon={<ShareIcon/>}/>
-        <Button size="md" variant="outline" label="Share" startIcon={<ShareIcon />} endIcon={<ShareIcon/>}/>
-        <Button size="lg" variant="outline" label="Share" startIcon={<ShareIcon />} endIcon={<ShareIcon/>}/>
+        <Card className="my-2">
+          <div className='text-black text-3xl font-bold text-center mb-4'>Login</div>
+          <div className='mb-10'>
+            <Input label="Email" size="lg" placeholder="Enter your email"/>
+            <Input label="Password" size="lg"placeholder="Enter your password" type="password"/>
+          </div>
+          <Button size="lg" variant="primary" label="Sign In"/>
+          <div className='text-center hover:underline'>
+            <a href="">Forgot Password?</a>
+          </div>
+        </Card>
+        <div className='text-brand-700 text-lg'>
+          Don't have an account? <a className='font-bold hover:underline' href="">Sign In</a>
+        </div>
       </div>
     </>
   );
--- frontend/src/ui/components/Button.jsx
+++ frontend/src/ui/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-const defaultStyles = 'flex items-center justify-center border-2'
+const defaultStyles = 'flex items-center justify-center border-2 my-4'
 const sizeStyle = {
-    'sm' : 'py-2 px-4 gap-2 rounded-md w-40',
-    'md' : 'py-2 px-6 gap-4 rounded-md w-60',
-    'lg' : 'py-2 px-8 gap-6 rounded-lg w-80'
+    'sm' : 'py-2 px-4 gap-2 rounded-md w-24 sm:w-48 lg:w-60',
+    'md' : 'py-2 px-6 gap-4 rounded-md w-36 sm:w-60 lg:w-80',
+    'lg' : 'py-2 px-8 gap-6 rounded-lg w-60 sm:w-80 lg:w-96'
 }
 const variantStyles = {
     'primary' : 'bg-brand-700 hover:bg-brand-800 text-brand-50',
@@ -13,7 +13,6 @@ const variantStyles = {
     'save' : 'bg-green-500 hover:bg-green-600 text-brand-50',
     'disabled' : 'bg-brand-100 text-brand-400'
 }
-
 
 export const Button = (props) => {
     const StartIcon = props.startIcon ? React.cloneElement(props.startIcon, {size : props.size}) : null;
